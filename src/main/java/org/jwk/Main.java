7520480da7a181bb512a06a69ea895f886b619f2
xyxlkb <xyxlkb@buaa.edu.cn>
2025-03-20 11:29:14 +0800

    合并

--- src/main/java/org/jwk/Main.java
+++ src/main/java/org/jwk/Main.java
@@ -1,32 +1,21 @@
 package org.jwk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "5113保护定制";
-        System.out.println(input.replaceAll("\\d+", ""));
-//        System.out.println("Hello world!");
-//        getNumberOrType(input);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2,100);
+        for (int i : list) {
+            System.out.println(i);
+        }
+
     }
 
-    public static void getNumberOrType(String currentLine) {
-        if (currentLine.contains("装置编号")) {
-            Pattern pattern = Pattern.compile("装置编号\\s*:?\\s*(\\S+)");
-            Matcher matcher = pattern.matcher(currentLine);
-            if (matcher.find()) {
-                String deviceNumber = matcher.group(1); // 获取第一个匹配的组（即\S+匹配的部分）
-                System.out.println(deviceNumber);
-            }
-        }
-        else if (currentLine.contains("装置型号") || currentLine.contains("保护型号")) {
-            Pattern pattern = Pattern.compile("装置型号\\s*(\\S+)");
-            Matcher matcher = pattern.matcher(currentLine);
-            if (matcher.find()) {
-                String deviceNumber = matcher.group(1); // 获取第一个匹配的组（即\S+匹配的部分）
-                System.out.println(deviceNumber);
-            }
-        }
-    }
 }
