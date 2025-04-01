616af268241ba77f0499a1b9c919711fb71c0d31
jiangwenkang3 <jiangwenkang3@jd.com>
2025-04-01 17:04:10 +0800

    20250401 lc1254 回去继续写

---
+++ src/main/java/org/jwk/lc1254_closedIsland.java
@@ -0,0 +1,23 @@
+package org.jwk;
+
+public class lc1254_closedIsland {
+}
+
+class Solution1254 {
+    int cnt = 0;
+    public int closedIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 0) {}
+        }
+
+
+        return cnt;
+    }
+
+
+}
